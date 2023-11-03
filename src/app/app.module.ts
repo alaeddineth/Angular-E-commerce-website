b96fc6f3c01d016a4e96104c61c9f94eea96e296
alaeddineth <theljanialaeddine@gmail.com>
2023-11-03 10:56:52 +0100

    "generating components"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AdminPanelComponent } from './admin-panel/admin-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductDetailComponent,
+    ShoppingCartComponent,
+    CheckoutComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent,
+    AdminPanelComponent
   ],
   imports: [
     BrowserModule,
